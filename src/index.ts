b0036a73d8325f8fb8fabb15aee33e1225e4476c
Aman Bhardwaj <inblack>
2020-12-11 21:40:14 +0530

    trust proxy

---
+++ src/index.ts
@@ -0,0 +1,91 @@
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
+import { Post } from './entities/Post';
+import { User } from './entities/User';
+import { Upvote } from './entities/Upvote';
+import { UserResolver } from './resolvers/user';
+import { isProd } from './utils/constants';
+import dotenv from 'dotenv';
+import 'colors';
+import { errorHandler } from './middlewares/errorHandler';
+import session from 'express-session';
+import Redis from 'ioredis';
+import connectRedis from 'connect-redis';
+import cors from 'cors';
+import { creatorLoader, voteLoader } from './utils/dataLoaders';
+
+const main = async () =>
+{
+    const RedisClient = new Redis( process.env.REDIS_URL );
+    const RedisStore = connectRedis( session );
+
+    dotenv.config( { path: 'config.env' } );
+
+    await createConnection( {
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        logging: true,
+        synchronize: true,
+        entities: [ Post, User, Upvote ],
+    } );
+
+    console.log( `Postgres with typeorm is here`.blue.bold );
+
+    const app = express();
+
+    // cookies must work in nginx env
+    app.set( 'trust proxy', 1 );
+
+    app.use( cors( {
+        credentials: true,
+        origin: process.env.CLIENT_URL
+    } ) );
+
+    app.get( '/', ( _, res ) =>
+    {
+        res.send( 'API up and running' );
+    } );
+
+    app.use(
+        session( {
+            store: new RedisStore( { client: RedisClient } ),
+            name: process.env.COOKIE_NAME,
+            secret: process.env.SESSION_SECRET,
+            resave: false, // no revival
+            saveUninitialized: false, // dont save until the cookie is generated
+            cookie: {
+                httpOnly: true,
+                sameSite: 'lax',
+                secure: isProd(),
+                maxAge: 1000 * 60 * 60 * 24, // 1 day
+                domain: isProd() ? '.vercel.app' : undefined
+            },
+        } ),
+    );
+
+
+    const apolloServer = new ApolloServer( {
+        schema: await buildSchema( {
+            resolvers: [ HelloResolver, PostResolver, UserResolver ],
+            validate: false
+        } ),
+        context: ( { req, res } ) => ( { req, res, creatorLoader: creatorLoader(), voteLoader: voteLoader() } )
+    } );
+
+    apolloServer.applyMiddleware( { app, cors: false } );
+
+    app.use( errorHandler );
+
+    const PORT = parseInt( process.env.PORT );
+    app.listen( PORT, () =>
+    {
+        console.log( `Server started on port ${ PORT }`.green.bold );
+    } );
+};
+
+main().catch( err => console.error( err ) );
